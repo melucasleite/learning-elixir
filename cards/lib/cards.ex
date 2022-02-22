d32832405b42f098911c614a34ce8e0b3788922d
Lucas Leite <me@lucasleite.us>
2022-02-22 15:16:42 -0500

    Add methods

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,33 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    for value <- values, suit <- suits do
+        "#{value} of #{suit}"
+    end
+  end
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+  def read(filename) do
+    case File.read(filename) do
+      {:ok, binary }-> :erlang.binary_to_term(binary)
+      {:error, reason} -> "That file doesn't exist: #{reason}"
+    end
+  end
+  def deal_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
