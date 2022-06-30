ea4eb54b14bf2ddcc6b657e624b06a29d4b2b3d0
Lucas <me@lucasleite.us>
2022-06-30 16:33:04 -0400

    Add tests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,33 +1,47 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods to deal a deck
+  """
+
+  @doc """
+    Returns a list of string representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
     for value <- values, suit <- suits do
-        "#{value} of #{suit}"
+      "#{value} of #{suit}"
     end
   end
+
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
+
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
   def read(filename) do
     case File.read(filename) do
-      {:ok, binary }-> :erlang.binary_to_term(binary)
+      {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, reason} -> "That file doesn't exist: #{reason}"
     end
   end
+
   def deal_hand(hand_size) do
-    Cards.create_deck
-    |> Cards.shuffle
+    Cards.create_deck()
+    |> Cards.shuffle()
     |> Cards.deal(hand_size)
   end
 end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,13 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create deck returns 20 cards" do
+    deck = Cards.create_deck()
+    assert length(deck) == 20
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    assert deck != Cards.shuffle(deck)
   end
 end
